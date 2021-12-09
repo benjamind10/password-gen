547b96e50b332217aec72a29ec47a863de77d9dc
benjamind10 <benjamind10@pm.me>
2021-12-08 22:25:15 -0500

    created two main functions to check length and other params

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,47 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', function () {
+  if (checkLen()) {
+    var upper = checkParams('Upper case');
+    console.log(upper);
+
+    var lower = checkParams('Lower case');
+    console.log(lower);
+
+    var num = checkParams('Numeric value');
+    console.log(num);
+
+    var symb = checkParams('Symbol');
+    console.log(symb);
+
+    if (!upper && !lower && !num && !symb) {
+      window.alert('You need to select at least 1');
+      return;
+    } else {
+      generatePassword();
+    }
+  } else {
+    return;
+  }
+});
+
+function generatePassword() {
+  console.log('working');
+}
+
+function checkLen() {
+  // New variable to keep track of password lenght
+  var len = window.prompt('Password length 8-128:');
+  // Conditional check to see if lenght is valid
+  if (len < 8) {
+    window.alert('You need to provide a value greater than 8.');
+    return false;
+  }
+  return true;
+}
+
+function checkParams(param) {
+  var checked = window.confirm('Select OK to include ' + param);
+  return checked;
+}
