f0a7abfdbc3dd8272f20985301b9f6b4ac3bfdc9
benjamind10 <benjamind10@pm.me>
2021-12-10 08:19:41 -0500

    refactoring logic

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,4 +1,5 @@
-// Universal Variables
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
 var len = 0;
 var params = [];
 var alpha = [
@@ -34,12 +35,9 @@ var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '='];
 var pass = '';
 var password = '';
 
-// Assignment Code
-var generateBtn = document.querySelector('#generate');
-
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(len);
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
@@ -50,45 +48,26 @@ generateBtn.addEventListener('click', function () {
   len = checkLen();
 
   if (len > 8) {
-    var upper = checkParams('Upper');
+    var upper = checkParams('Upper case');
     params.push(upper);
-    var lower = checkParams('Lower');
+    var lower = checkParams('Lower case');
     params.push(lower);
-    var lower = checkParams('Numbers');
-    params.push(lower);
-    var lower = checkParams('Symbols');
-    params.push(lower);
-
+    var num = checkParams('Numeric value');
+    params.push(num);
+    var symb = checkParams('Symbol');
+    params.push(symb);
     if (!upper && !lower && !num && !symb) {
       window.alert('You need to select at least 1');
       return;
     } else {
-      //   generatePassword(len);
+      generatePassword(len);
     }
   } else {
     return;
   }
+  writePassword(len);
 });
 
-function generatePassword() {
-  return 'test';
-}
-
-// Check params fn`
-function checkParams(param) {
-  // Variable to check the current parameter
-  if (param === 'Upper' || param === 'Lower') {
-    var checked = window.confirm('Select OK to include ' + param + ' case');
-  } else {
-    var checked = window.confirm('Select OK to include ' + param);
-  }
-  if (checked) {
-    return param;
-  }
-  return false;
-}
-
-// Check length fn
 function checkLen() {
   // New variable to keep track of password lenght
   var len = window.prompt('Password length 8-128:');
@@ -98,4 +77,25 @@ function checkLen() {
     return false;
   }
   return len;
+}
+
+function checkParams(param) {
+  // Variable to check the current parameter
+  var checked = window.confirm('Select OK to include ' + param);
+  return checked;
+}
+
+function generatePassword(len, params) {
+  for (var i = 0; i < params.length; i++) {}
+  var final = randomizer(alpha, 4, len);
+  return final;
+}
+
+function randomizer(type, params, len) {
+  for (var i = 0; i < type.length; i++) {
+    var tmp = Math.floor(Math.random() * type.length);
+    pass += type[tmp];
+  }
+  password += pass.slice(0, Math.ceil(len / 4));
+  return password;
 }
