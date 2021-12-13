39c5da38e706136b9c283b13f70b1d100dd879f0
Ben <benjamind10@pm.me>
2021-12-13 09:11:36 -0500

    added new html and css to remove prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 // Universal Variables
 var len = 0;
 var params = [];
-var pass = '';
+var tmp = '';
+var finalKey = [];
+
+// Query selectors
+var slider = document.getElementById('myRange');
+var output = document.getElementById('slider-length');
+
+// Controls slider behavior
+var length = 0;
+output.innerHTML = 'Length: ' + slider.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = 'Length: ' + this.value;
+  length = parseInt(this.value);
+};
 
 // Object that holds the kes
 var keys = {
@@ -20,8 +34,9 @@ function writePassword() {
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
+
   // Resets values for new check;
-  pass = '';
+  tmp = '';
   params = [];
   len = 0;
 }
@@ -31,48 +46,52 @@ generateBtn.addEventListener('click', function () {
   // Resets parameters if no selection was made
   params = [];
   // Asks useer for lenght of password
-  len = parseInt(
-    window.prompt('Enter password length between 8 and 128 chars.')
-  );
+  len = parseInt(prompt('Enter password length between 8 and 128 chars.'));
   // Conditional checks for appropiate format.
   if (!len) {
-    window.alert('This needs to be a number.');
+    alert('This needs to be a number.');
     return;
   } else if (len < 8 || len > 128) {
-    len = window.alert('You need to provide a value between 8 and 128.');
+    len = alert('You need to provide a value between 8 and 128.');
     return;
   } else {
     // Checks the parameters
     var upperCase = window.confirm('Include upper case?');
     params.push(upperCase);
+
     var lowerCase = window.confirm('Include lower case?');
     params.push(lowerCase);
+
     var numbers = window.confirm('Include numbers?');
     params.push(numbers);
+
     var symbols = window.confirm('Include symbols?');
     params.push(symbols);
   }
   // Conditional to check if at least 1 parameter was selected.
   if (!upperCase && !lowerCase && !numbers && !symbols) {
-    window.alert('You need to select at least one');
+    alert('You need to select at least one');
     return;
   } else {
+    // If all conditionals pass it wirtes pw to screen
     writePassword();
   }
 });
 
+// Function that generates the password
 function generatePassword(len, key) {
-  console.log(len, key);
-  while (len > pass.length) {
+  // Loops while the tmp pw is less than the length povided
+  while (len > tmp.length) {
+    // Picks a random function from the key array
     var addKey = key[Math.floor(Math.random() * key.length)];
-    pass += addKey();
+    tmp += addKey();
   }
-  return pass;
+  return tmp;
 }
 
 // Function to select which keys to use
 function getKeys(params) {
-  // Full Array with all parameters enabled
+  // Array with all the functions to randomize characters
   var getKey = [
     function upperCase() {
       return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
@@ -88,69 +107,14 @@ function getKeys(params) {
     },
   ];
 
-  // Checks to validate each parameter and select the ones that are true
-  if (params[0] && params[1] && params[2] && params[3]) {
-    getKey = getKey;
-  } else if (params[1] && params[2] && params[3]) {
-    var getKey = [
-      function lowerCase() {
-        return keys.lowerCase[
-          Math.floor(Math.random() * keys.lowerCase.length)
-        ];
-      },
-      function number() {
-        return keys.number[Math.floor(Math.random() * keys.number.length)];
-      },
-      function symbol() {
-        return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
-      },
-    ];
-  } else if (params[0] && params[2] && params[3]) {
-    var getKey = [
-      function upperCase() {
-        return keys.upperCase[
-          Math.floor(Math.random() * keys.upperCase.length)
-        ];
-      },
-      function number() {
-        return keys.number[Math.floor(Math.random() * keys.number.length)];
-      },
-      function symbol() {
-        return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
-      },
-    ];
-  } else if (params[0] && params[1] && params[3]) {
-    var getKey = [
-      function upperCase() {
-        return keys.upperCase[
-          Math.floor(Math.random() * keys.upperCase.length)
-        ];
-      },
-      function lowerCase() {
-        return keys.lowerCase[
-          Math.floor(Math.random() * keys.lowerCase.length)
-        ];
-      },
-      function symbol() {
-        return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
-      },
-    ];
-  } else {
-    var getKey = [
-      function upperCase() {
-        return keys.upperCase[
-          Math.floor(Math.random() * keys.upperCase.length)
-        ];
-      },
-      function lowerCase() {
-        return keys.lowerCase[
-          Math.floor(Math.random() * keys.lowerCase.length)
-        ];
-      },
-      function number() {
-        return keys.number[Math.floor(Math.random() * keys.number.length)];
-      },
-    ];
+  // Resets final keys if app ran again
+  finalKey = [];
+
+  // Loops the params array and selects the corresponding functions
+  for (var i = 0; i < params.length; i++) {
+    if (params[i]) {
+      finalKey.push(getKey[i]);
+    }
   }
-  return getKey;
+  return finalKey;
 }
